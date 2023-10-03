60d6ed58dd4bed5d5dbc6f20e0c48fa4948cb02c
Hiago Alves <hiagotnt1@gmail.com>
2023-10-03 03:14:11 -0300

    feat: logging and monitoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,21 @@ import { MailModule } from './mail/mail.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user/user.module';
 import { SessionModule } from './modules/session/session.module';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { RolesGuard } from './common/guards/roles.guard';
 import { AuthGuard } from './modules/auth/auth.guard';
 import { CacheModule } from './connections/cache/cache.module';
 import { ProfileModule } from './modules/profile/profile.module';
 import { AssetModule } from './modules/asset/asset.module';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PrometheusModule.register(),
     AdminModule,
     AuthModule,
     CustomerModule,
@@ -33,6 +36,7 @@ import { AssetModule } from './modules/asset/asset.module';
   providers: [
     { provide: APP_GUARD, useClass: AuthGuard },
     { provide: APP_GUARD, useClass: RolesGuard },
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
   ],
 })
 export class AppModule {}
